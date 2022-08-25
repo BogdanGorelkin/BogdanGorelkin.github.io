ef2070d833a9e9102f62867b628cc1b974efcf97
b.gorelkin <b.k.gorelkin@gmail.com>
2022-08-25 17:16:06 +0200

    1st page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { HashRouter, Route, Routes } from 'react-router-dom'
+import MainPage from './features/MainPage'
+import './App.css';
+
+function App() {
+  return (
+    <HashRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<MainPage />} />
+    </Routes>
+  </HashRouter>
+  )
+}
+
+export default App;
